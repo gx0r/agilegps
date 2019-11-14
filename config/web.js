5bc6a7b5a66c8e2882493ec19c6b07f1d8015176
Grant Miner <llambda@users.noreply.github.com>
2019-11-13 18:13:06 -0600

    Indent fix

--- config/web.js
+++ config/web.js
@@ -1,17 +1,17 @@
 'use strict';
 
 module.exports = {
-    "proxy": false,
-    "isReverseProxied": false,
-    "port": 3000,
-    "tlsport": 3443,
-    "cluster": false,
-    "spdy": false,
-    "https": false,
-    "jwtSecret": "seecret",
-    "cookieKeys": [
-        "seecret1",
-        "seecret2"
-    ],
-	"longStackTraces": true
+  "proxy": false,
+  "isReverseProxied": false,
+  "port": 3000,
+  "tlsport": 3443,
+  "cluster": false,
+  "spdy": false,
+  "https": false,
+  "jwtSecret": "seecret",
+  "cookieKeys": [
+    "seecret1",
+    "seecret2"
+  ],
+  "longStackTraces": true
 }
