8f643320f31b9d7138dd904d6e1402f18b457830
Grant Miner <ggcode1@users.noreply.github.com>
2019-11-15 17:40:26 -0600

    cleanup

--- src/client/components/map.js
+++ src/client/components/map.js
@@ -27,52 +27,25 @@ class Map extends React.Component {
   };
 
   static propTypes = {
-    // orgName: PropTypes.string,
-    // user: PropTypes.object,
   };
 
-  // componentDidMount() {
-  //   const el = this.myRef.current;
-  //   debugger;
-  //   if (!mounted) {
-  //     mounted = true;
-  //     // debugger;
-  //     // el.style.height = "400px";
-  //     el.appendChild(new google.maps.Map(el, {
-  //       zoom: 4,
-  //       center: { lat: 50, lng: -98.35 },
-  //       rotateControl: true
-  //     }));
-
-  //   }
-  // }
-
-
-  createMarker = item => {
+  createMarker = vehicle => {    
     const map = this.map.map;
-    let position;
-    if (item.last) {
-      position = toGoogle(item.last);
-    } else {
-      position = toGoogle(item);
-    }
-  
-    if (!position) {
-      console.warn("Org marker, invalid position " + JSON.stringify(item));
-      return;
-    }
+    const position = toGoogle(vehicle.last ? vehicle.last : item);
   
     const marker = new MarkerWithLabel({
-      position: position,
-      map: map,
-      title: item.name,
-      labelContent: item.name,
+      position,
+      map,
+      title: vehicle.name,
+      labelContent: vehicle.name,
       labelAnchor: new google.maps.Point(0, 0),
       labelClass: "maplabel", // the CSS class for the label
       labelInBackground: false
     });
   
-    marker.setIcon(Status.getMarkerIconFleetView(item.last));
+    marker.setIcon(Status.getMarkerIconFleetView(vehicle.last));
+
+    // TODO
     // google.maps.event.addListener(
     //   marker,
     //   "click",
@@ -83,25 +56,18 @@ class Map extends React.Component {
   }
 
   populateMapMarkers = () => {
-    const { impliedSelectedVehicles, selectedFleets, selectedFleetsAll, vehiclesByID } = this.props;
+    const { impliedSelectedVehicles, vehiclesByID } = this.props;
     const { markersByVehicleID } = this;
 
-    if (impliedSelectedVehicles.length > 0) {
-      const bounds = new google.maps.LatLngBounds();
+    const bounds = new google.maps.LatLngBounds();
 
-      impliedSelectedVehicles.forEach(vehicle => {
-        if (vehiclesByID[vehicle.id]) {
-          const marker = this.createMarker(vehiclesByID[vehicle.id], false);
-          if (marker) {
-            bounds.extend(marker.position);
-            if (markersByVehicleID[vehicle.id]) {
-              markersByVehicleID[vehicle.id].setMap(null); // TODO why necessary
-            }
-            markersByVehicleID[vehicle.id] = marker;
-          }
-        }
-      });
-    }
+    impliedSelectedVehicles.forEach(vehicle => {
+      if (vehiclesByID[vehicle.id] && vehiclesByID[vehicle.id].last) {
+        const marker = this.createMarker(vehiclesByID[vehicle.id], false);
+        bounds.extend(marker.position);
+        markersByVehicleID[vehicle.id] = marker;
+      }
+    });
   }
 
   handleApiLoaded = (map, maps) => {
@@ -115,6 +81,7 @@ class Map extends React.Component {
   }
 
   componentWillUpdate() {
+    // this.removeMapMarkers();
     this.populateMapMarkers();
   }
 
@@ -127,8 +94,7 @@ class Map extends React.Component {
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
           onGoogleApiLoaded={ this.handleApiLoaded } 
-        >
-          
+        >          
         </GoogleMapReact>
       </div>
     );
@@ -141,8 +107,6 @@ export default connect(
     selectedFleets: state.selectedFleets,
     selectedFleetsAll: state.selectedFleetsAll,
     vehiclesByID: state.vehiclesByID,
-    // orgName: state.selectedOrg.name,
-    // user: state.user,
   }),
   dispatch => bindActionCreators({
   }, dispatch),
