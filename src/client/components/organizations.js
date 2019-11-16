0d4c855c38b5083f20a3a8257412172c676e5044
Grant Miner <llambda@users.noreply.github.com>
2019-11-15 23:19:26 -0600

    Fix catchhandler

--- src/client/components/organizations.js
+++ src/client/components/organizations.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import classnames from 'classnames';
+import catchhandler from '../catchhandler';
 
 import appState from '../appState';
 
