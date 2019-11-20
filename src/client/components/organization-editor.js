3f198c2029a54ae5f09b2098bb80c7d60f12687c
Grant Miner <ggcode1@users.noreply.github.com>
2019-11-19 23:45:47 -0600

    Fixup edit layout

--- src/client/components/organization-editor.js
+++ src/client/components/organization-editor.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import classnames from 'classnames';
 
+import * as appState from '../appState';
 import * as Organization from "../../common/models/Organization";
 
 class OrganizationEditor extends React.Component {
@@ -12,6 +13,7 @@ class OrganizationEditor extends React.Component {
     super(props);
     this.state = {
       editing: true,
+      changedValues: {},
     }
   }
 
@@ -22,38 +24,59 @@ class OrganizationEditor extends React.Component {
   componentWillMount() {
   }
 
+  save = () => {
+    const { org } = this.props;
+    appState.saveOrg(org)
+    .then(() => {
+      toast.success("Org saved");
+    })
+    .catch(err => {
+      toast.error(err.message);
+    });
+  }
+
   render() {
     const { org } = this.props;
 
     return (
-      <div className="business-table">
+      <div>
         <div className="col-sm-3" />
         <div className="business-table col-sm-6">
           <div className="btn center">Edit Organization</div>
-        </div>
-        <form className="form-horizontal">
-          {
-            Object.keys(org).map(key => {
-              return (
-                <div className="form-group">
-                  <label className="col-md-2 control-label">{ key }</label>
-                  <div className="div.col-md-10">
-                    <div className="input.form-control">
-                    { org[key] }
+          <form className="form-horizontal">
+            {
+              Object.keys(org).map(key => {
+                return (
+                  <div className="form-group">
+                    <label className="col-md-2 control-label">{ key }</label>
+                    <div className="col-md-10">
+                      <input
+                        className="form-control"
+                        disabled={ key === "id" }
+                        onChange={ ev => org[key] = ev.target.value }
+                        value={ org[key] }
+                      />
                     </div>
-                  </div>                
-                </div>
-              );
-            })
-          }
+                  </div>
+                );
+              })
+            }
+            <div className="buttons-right">
+            <button
+              className="btn btn-default"
+              onClick={ window.history.back() }
+            >Cancel</button>
+          </div>
+          </form>
           <div className="buttons-right">
-          <button
-            className="btn btn-default"
-            onClick={ window.history.back() }
-          >Cancel</button>
-          <button className="btn btn-success">Save</button>
+            <button
+              className="btn btn-success"
+              onClick={ this.save }
+            >
+                Save
+            </button>
+          </div>
         </div>
-        </form>        
         <div className="col-sm-3" />
       </div>
     );
