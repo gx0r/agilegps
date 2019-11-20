39b904feb38fc2848bd2949adc646e18a4b30bf3
Grant Miner <llambda@users.noreply.github.com>
2019-11-19 21:10:32 -0600

    Add UDP message relaying

--- config/listener.js
+++ config/listener.js
@@ -1,9 +1,17 @@
 'use strict';
 
 module.exports = {
-    "port": 33333, // listen port
-    "host": "0.0.0.0", // listen on host
-    "insertrawevents": true, // insert the raw events to the DB
-    "geocode": true, // reverse geocode on or off
-    "sack": true // send SACK on or off
+    host: "0.0.0.0", // listen on host
+    port: 33333, // UDP listen port
+
+    relayTo: [
+      // forward/relay messages to additional UDP sockets
+      { host: "localhost", port: 8871 }
+    ],
+
+    insertrawevents: true, // insert the raw events to the DB
+
+    geocode: true, // reverse geocode on or off
+    sack: true, // send SACK on or off
+
 }
