adfc7dc4136c6438fe4d623cc74e7610e9da297a
Grant Miner <llambda@users.noreply.github.com>
2019-11-21 17:51:19 -0600

    Local date parsing

--- src/client/formatDate.js
+++ src/client/formatDate.js
@@ -3,10 +3,15 @@ const isUserMetric = require("./isUserMetric");
 const moment = require("moment");
 
 function formatDate(item) {
-  if (isUserMetric()) {
-    return moment(item).format("YYYY-MM-DD HH:mm:ss");
-  } else {
-    return moment(item).format("M/DD/YYYY h:mm:ss A");
+  try {
+    if (isUserMetric()) {
+      return moment(item).format("YYYY-MM-DD HH:mm:ss");
+    } else {
+      return moment(item).format("M/DD/YYYY h:mm:ss A");
+    }
+  } catch (err) {
+    console.warn(err);
+    return item;
   }
 }
 
