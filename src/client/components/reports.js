4747303356a27b863b81d28bd86f6ef58ba38bcf
Grant Miner <ggcode1@users.noreply.github.com>
2019-11-24 21:56:19 -0600

    Hookup mileage report

--- src/client/components/reports.js
+++ src/client/components/reports.js
@@ -11,20 +11,61 @@ import { toast } from 'react-toastify';
 import * as appState from '../appState';
 import * as tzOffset from "../tzoffset";
 import { auth, validateResponse } from '../appState';
+import * as isUserMetric from "../isUserMetric";
+import tomiles from '../tomiles';
 
 const reports = {
+  mileage: null,
   idle: null,
   obd: null,
 };
+
+function Mileage({results, vehicles}) {
+  return (
+    <div>
+      <table className="table-condensed table-bordered table-striped dataTable">
+        <thead>
+          <tr>
+            <td>State</td>
+            <td>{ isUserMetric() ? 'Kilometers' : 'Miles' }</td>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            Object.keys(vehicles).map(vid => {
+              return <>
+                <tr key={vid}>
+                  <td colSpan="7" className="group">
+                    { vehicles[vid].name }
+                  </td>
+                </tr>
+                {
+                  Object.keys(results[vid]).map(key => {
+                    return <tr>
+                      <td>{key}</td>
+                      <td>{tomiles(results[vid][key])}</td>
+                    </tr>
+                  })
+                }
+              </>
+            })
+          }
+        </tbody>
+      </table>
+    </div>
+  )
+}
 
 
 function Reports({ impliedSelectedVehiclesByID, orgsByID, vehiclesByID }) {
   const { orgId } = useParams();
   const [focusedInput, setFocusedInput] = useState(null);
   const [executing, setExecuting] = useState(false);
-  const [reportType, setReportType] = useState('idle');
+  const [executed, setExecuted] = useState(false);
+  const [reportType, setReportType] = useState('mileage');
   const [results, setResults] = useState({});
-  const [startDate, setStartDate] = useState(moment());
+  const [resultVehicles, setResultVehicles] = useState({});
+  const [startDate, setStartDate] = useState(moment().subtract(1, 'day'));
   const [endDate, setEndDate] = useState(moment());
 
   const execute = () => {
@@ -34,9 +75,12 @@ function Reports({ impliedSelectedVehiclesByID, orgsByID, vehiclesByID }) {
     fetch(`/api/organizations/${orgId}/reports/${encodeURIComponent(reportType)}?vehicles=${encodeURIComponent(JSON.stringify(ids))}&startDate=${encodeURIComponent(startDate.toISOString())}&endDate=${encodeURIComponent(
           moment(endDate).add(1, "day").toISOString())}&tzOffset=${encodeURIComponent(tzOffset())}`, auth())
       .then(validateResponse)
-      .then(results => {
-        setResults(results);
+      .then(response => {
+        setResultVehicles(response.vehicles);
+        setResults(response.results);
+        // setResultsTotals(response.totals);
         setExecuting(false);
+        setExecuted(true);
       })
       .catch(function(err) {
         toast.error(err.message);
@@ -89,6 +133,9 @@ function Reports({ impliedSelectedVehiclesByID, orgsByID, vehiclesByID }) {
           </div>
       </div>
       <div className="col-sm-3" />
+      <div className="row col-md-12">
+        { executed && <Mileage results={results} vehicles={resultVehicles} />}
+      </div>
     </div>
   );
 }
